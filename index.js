7b260bad5a748788ab0b75a6dc1c53e00fe281b7
Maria Panteleichuk <varvarich65@gmail.com>
2024-09-05 11:21:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ window.addEventListener("load",()=>{
     fetch("data/car.json")
         .then((res)=>res.json())
         .then((car)=>{
-            fetch("../hbs/car.hbs")
+            fetch("hbs/car.hbs")
             .then((res)=>res.text())
             .then((res)=>{
                 res = Handlebars.compile(res)
